9f1477355c8225d758aab144c95088024db8246f
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-22 17:47:23 +0200

    Structure html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <img src='./logo.png' alt='logo' title='Logo de Rick y Morty'></img>
+      <ul></ul>
+    </>
+  );
 };
 export default App;
