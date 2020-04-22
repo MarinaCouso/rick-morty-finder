3971bde2b2734869ad42264dcbea3e84fbf7bfe2
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-22 17:26:18 +0200

    Run docs

---
+++ docs/static/js/main.dc5ef170.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp3.3.5"]=this["webpackJsonp3.3.5"]||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),c=t(2),o=t.n(c),u=(t(8),function(){return a.a.createElement(a.a.Fragment,null)});o.a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(u,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.dc5ef170.chunk.js.map
