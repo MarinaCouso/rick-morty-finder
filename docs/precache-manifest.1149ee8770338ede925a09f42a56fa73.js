e4d7f4ac97147464943beceace6c58e2f732fdd2
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-22 18:57:51 +0200

    Run docs

---
+++ docs/precache-manifest.1149ee8770338ede925a09f42a56fa73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e488a965adee9bf65fe824e40557d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48af9ba15caee0e4620",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "bca3d4f5bae0fd911ff5",
+    "url": "/static/js/2.520284b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.520284b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48af9ba15caee0e4620",
+    "url": "/static/js/main.2e731be5.chunk.js"
+  },
+  {
+    "revision": "719c4a20207bc1186a02",
+    "url": "/static/js/runtime-main.23db867c.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/static/media/logo.75509f2e.png"
+  }
+]);
