68a07d244c4a89513cdbaed9bbd68bc1b2075d81
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-24 11:35:21 +0200

    Advance CharacterDetail SASS

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,29 +3,30 @@ import React from 'react';
 const CharacterDetail = (props) => {
   console.log(props);
   return (
-    <>
-      <a className='article__link' href={`'./character/${props.character.id}'`}>
+    <div className='article__box'>
+      <a className='article__link__text' href={`'./character/${props.character.id}'`}>
         {'< Volver'}
       </a>
+
       <article className='article' key={props.character.id}>
         <img className='article__img' src={props.character.image} alt={props.character.name} title={`picture of ${props.character.name}`}></img>
         <div className='article__text'>
           <h2 className='article__name'>{props.character.name}</h2>
           <p className='article__attribute'>
-            <span className='article__attribute__bold'>Status:</span> {props.character.status}
+            <span className='article__attribute__title'>Status:</span> {props.character.status}
           </p>
           <p className='article__attribute'>
-            <span className='article__attribute__bold'>Species:</span> {props.character.specie}
+            <span className='article__attribute__title'>Species:</span> {props.character.specie}
           </p>
           <p className='article__attribute'>
-            <span className='article__attribute__bold'>Origin:</span> {props.character.origin}
+            <span className='article__attribute__title'>Origin:</span> {props.character.origin}
           </p>
           <p className='article__attribute'>
-            <span className='article__attribute__bold'>Episodes:</span> {props.character.episodes}
+            <span className='article__attribute__title'>Episodes:</span> {props.character.episodes}
           </p>
         </div>
       </article>
-    </>
+    </div>
   );
 };
 
