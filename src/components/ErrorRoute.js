80cb895af64e4921a98316b6a30d2d19262cd492
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-25 11:29:55 +0200

    Add home link at ErrorRoute and Error Search

--- src/components/ErrorRoute.js
+++ src/components/ErrorRoute.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 const ErrorRoute = (props) => {
   return (
-    <Link to={`/character/${props.search}`}>
+    <>
       <p>{`No hay ningún personaje que coincida con la ruta /character/${props.id}`}</p>
-    </Link>
+      <a className='article__link' href='/'>
+        {'< Volver'}
+      </a>
+    </>
   );
 };
 
--- src/components/ErrorSearch.js
+++ src/components/ErrorSearch.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 const ErrorSearch = (props) => {
   return (
-    <Link to={`/character/${props.search}`}>
+    <>
       <p>{`No hay ningún personaje que coincida con ${props.search}`}</p>
-    </Link>
+      <a className='article__link' href='/'>
+        {'< Volver'}
+      </a>
+    </>
   );
 };
 
