3f756d99225894cfa61792b1bb3465595e789daa
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-25 13:28:08 +0200

    Fix detail at ErrorSearch text

--- src/components/ErrorSearch.js
+++ src/components/ErrorSearch.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorSearch = (props) => {
   return (
     <div className='error'>
-      <p className='error__text'>{`No hay ningún personaje que coincida con ${props.search}`}</p>
+      <p className='error__text'>{`No hay ningún personaje que coincida con '${props.search}'`}</p>
       <a className='error__link' href='/'>
         {'< Volver'}
       </a>
